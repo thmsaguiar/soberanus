429cdc55661a73af8b6d004f37d0b99ad100f56a
Wellington de Almeida <62154799+otonalmeidas@users.noreply.github.com>
2021-09-28 02:15:30 -0300

    Upgrading layout

--- src/app/initial/initial.page.ts
+++ src/app/initial/initial.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 // Interface Produtos
 interface IProducts{
   name: string;
+  description: string;
   price: number;
   quantidade: number;
 }
@@ -16,28 +17,33 @@ interface IProducts{
 export class InitialPage implements OnInit {
   public products: IProducts [] = [
     {
-      name: 'Hot-Dog',
+      name: '01. Hot-Dog',
+      description: 'Salsicha viena, batata palha, cheddar, farofa de bacon catchup e mostarda.',
       price: 10,
       quantidade: 0
     },
     {
-      name: 'X-Salada',
+      name: '02. X-Salada',
+      description: 'Burger 120g angus, american cheese, picles, alface americano, tomate e molho especial.',
       price: 12,
       quantidade: 0
     },
     {
-      name: 'X-Bacon',
+      name: '03. X-Bacon',
+      description: 'Burger 120g angus, american cheese, fatias de bacon crocantes e cebola caramelisada.',
       price: 14,
       quantidade: 0
     },
     {
-      name: 'X-Tudo',
+      name: '04. X-Tudo',
+      description: 'Burger 120g angus, bacon, presunto, 4 queijos, ovo, picles, alface americano, tomate e molho especial.',
       price: 18,
       quantidade: 0
     },
     {
-      name: 'X-Vegan',
-      price: 8,
+      name: '05. X-Vegan',
+      description: 'Burger falafel, queijo de mandioca, rucula, tomate cereja e molho especial vegano.',
+      price: 16,
       quantidade: 0
     }
   ];
