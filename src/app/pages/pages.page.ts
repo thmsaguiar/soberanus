fc1e2eb0f66e2596bf3933470048d120d6989101
thmsaguiar <thvicente110100@gmail.com>
2021-10-06 20:55:17 -0300

    Adicionando alert no botão de logout

--- src/app/pages/pages.page.ts
+++ src/app/pages/pages.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController, public router:Router) { }
 
   ngOnInit() {
+  }
+
+  async exit(){            
+    const alert = await this.alertCtrl.create({
+      header: 'Finalizando sessão!',
+      message: 'Deseja realmente sair?',
+      buttons: [
+      {
+        text: 'Não',
+        role: 'cancel'        
+      },
+      {
+        text: 'Sim',
+        handler: () => {
+          this.router.navigateByUrl('/home');
+        }
+      }
+    ]
+    });
+    alert.present();    
+    
   }
 
 }
