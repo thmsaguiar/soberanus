0b1b077c7f3de32cf3dd9d86404edaf5c27f4c28
thmsaguiar <thvicente110100@gmail.com>
2021-10-30 13:05:48 -0300

    armazenamento offline de usuarios

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { comparaValidator } from './../validators/compara-validator';
+import { Usuario } from './../models/Usuario';
+import { StorageService } from './../services/storage.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -13,7 +15,11 @@ export class CadastroPage implements OnInit {
 
   formCadastro: FormGroup;   
 
-  constructor(public router:Router, private formBuilder: FormBuilder, private alertCtrl: AlertController) { 
+  usuario: Usuario = new Usuario();  
+
+  listaUser: Usuario[] = [];
+
+  constructor(public route:Router, private formBuilder: FormBuilder, private alertCtrl: AlertController, private storageService: StorageService) { 
     this.formCadastro = this.formBuilder.group({
       user: ['', Validators.required],
       password: ['', Validators.required],
@@ -28,15 +34,43 @@ export class CadastroPage implements OnInit {
   }
 
   async save(){
-    console.log('Formulário: ', this.formCadastro.valid);
-    
-    const alert = await this.alertCtrl.create({
-        header: 'Cadastrado!',
-        message: 'Usuário cadastrado com sucesso.',
-        buttons:['OK']
-      });
-      alert.present(); 
-    this.router.navigateByUrl('/login');   
+    var tem = false;
+    if(this.formCadastro.valid){
+      for(let users of this.listaUser){
+        if(users.user == this.usuario.user){
+          tem = true;
+        }
+      }
+      if(tem == false){
+        this.usuario.user = this.formCadastro.value.user;
+        this.usuario.password = this.formCadastro.value.password;
+        await this.storageService.set(this.usuario.user, this.usuario);
+        const alert = await this.alertCtrl.create({
+          header: 'Cadastrado!',
+          message: 'Usuário cadastrado com sucesso.',
+          buttons:['OK']
+        });
+        alert.present();         
+        this.route.navigateByUrl('/login'); 
+      }else{
+        const alert = await this.alertCtrl.create({
+          header: 'Usuário já cadastrado!',
+          message: 'Por favor utilize outro nome de usuário.',
+          buttons:['OK']
+        });
+        alert.present();
+      } 
+    }else{
+      alert('Formulário inválido!');
+    }
   }
+
+  async buscarUsuarios(){
+    this.listaUser = await this.storageService.getAll();
+  }
+
+  ionViewDidEnter(){
+    this.buscarUsuarios();
+  }  
 
 }
---
+++ src/app/models/Usuario.ts
@@ -0,0 +1,4 @@
+export class Usuario {
+	user: string;
+	password: string;
+}
