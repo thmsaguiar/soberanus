f6a22de0e2299df83f55100057b16c8537ee5fcc
thmsaguiar <thvicente110100@gmail.com>
2021-10-30 13:31:23 -0300

    verificando dados de login com usuarios cadastrados

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,46 +5,79 @@ import { AlertController } from '@ionic/angular';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { LoginPageForm } from './login.page.form';
 
+import { Usuario } from './../models/Usuario';
+import { StorageService } from './../services/storage.service';
+
 
 //Interface Login
-interface ILogin{
+/*interface ILogin{
   user: string;
   password: string;
-}
+}*/
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-  public user: string;
-  public password: string;
-  public loginUser: ILogin = {
-    user: 'admin',
-    password: 'admin'
-  };
+export class LoginPage implements OnInit {  
+
   public form: FormGroup;
 
-  constructor(public router:Router, private formBuilder: FormBuilder,private alertCtrl: AlertController) { }
+  usuario: Usuario = new Usuario();
+
+  listaUser: Usuario[] = [];
+
+  public valido = false;  
+
+  constructor(public router:Router, private formBuilder: FormBuilder,private alertCtrl: AlertController, private storageService: StorageService) { }
 
   ngOnInit() {
     this.form = new LoginPageForm(this.formBuilder).createForm();
   }
 
-  async goToInitial(){        
-    this.user = this.form.get('user').value;
-    this.password = this.form.get('password').value;
-    if((this.user == this.loginUser.user) && (this.password == this.loginUser.password)){
-      this.router.navigateByUrl('/pages/initial');          
-    }else{
+  async goToInitial(){     
+    this.verificarUser();   
+    if(this.valido == true){
+      this.router.navigateByUrl('/pages/initial');      
+    }            
+  }  
+
+  async verificarUser(){    
+    this.usuario.user = this.form.get('user').value;
+    this.usuario.password = this.form.get('password').value;
+    var tem = false;            
+    for(let users of this.listaUser){
+      if(users.user == this.usuario.user){
+        tem = true;
+        if(users.password == this.usuario.password){
+          this.valido = true;
+        }else{
+          const alert = await this.alertCtrl.create({
+            header: 'Senha inválida!',
+            message: 'Tente novamente.',
+            buttons:['OK']
+          });
+          alert.present();    
+        }
+      }
+    }
+    if(tem == false){
       const alert = await this.alertCtrl.create({
-        header: 'Usuário ou senha inválidos!',
-        message: 'Tente novamente.',
+        header: 'Usuário inválido!',
+        message: 'Este usuário não está cadastrado.',
         buttons:['OK']
       });
       alert.present();    
     }    
-  }  
+  }
+
+  async buscarUsuarios(){
+    this.listaUser = await this.storageService.getAll();
+  }
+
+  ionViewDidEnter(){
+    this.buscarUsuarios();
+  }
 
 }
