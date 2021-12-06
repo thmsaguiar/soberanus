718bf13235286b1e1459551565dae853d2d4718b
thmsaguiar <thvicente110100@gmail.com>
2021-12-06 13:55:48 -0300

    correcoes page record

--- src/app/pages/record/record.page.ts
+++ src/app/pages/record/record.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Product } from 'src/app/models/Product';
 import { Venda } from 'src/app/models/Venda';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -15,9 +14,9 @@ export class RecordPage implements OnInit {
   listaVendas: Venda[] = [];
 
   constructor(private storageService: StorageService, public router: ActivatedRoute){}
-  
+
   async buscarVendas(){
-    this.listaVendas = await this.storageService.getAll();    
+    this.listaVendas = await this.storageService.getAll();
   }
 
   ionViewDidEnter(){
@@ -27,19 +26,19 @@ export class RecordPage implements OnInit {
   async excluirVenda(id: string){
     await this.storageService.remove(id);
     this.buscarVendas();
-  }  
+  }
 
-  gerarData(data: Date){        
-    var dia = String(data.getDate()).padStart(2, '0');
-    var mes = String(data.getMonth() + 1).padStart(2, '0');
-    var ano = data.getFullYear();
-    var hora = data.getHours();
-    var min = data.getMinutes();
-    var dataAtual = dia + '/' + mes + '/' + ano+'   Hora: '+hora+':'+min;
+  gerarData(data: Date){
+    const dia = String(data.getDate()).padStart(2, '0');
+    const mes = String(data.getMonth() + 1).padStart(2, '0');
+    const ano = data.getFullYear();
+    const hora = data.getHours();
+    const min = data.getMinutes();
+    const dataAtual = dia + '/' + mes + '/' + ano+'   Hora: '+hora+':'+min;
     return dataAtual;
   }
 
-  ngOnInit() {    
+  ngOnInit() {
   }
 
 }
