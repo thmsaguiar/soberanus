75ad71bd3604b3cdaed53338de75dab69921e521
thmsaguiar <thvicente110100@gmail.com>
2021-12-06 13:59:29 -0300

    alterando rotas das paginas

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: PagesPage,
-    children: [      
+    children: [
       {
         path: 'initial',
         loadChildren: () => import('./initial/initial.module').then( m => m.InitialPageModule)
@@ -19,9 +19,18 @@ const routes: Routes = [
       {
         path: 'detalhes/:id',
         loadChildren: () => import('./detalhes/detalhes.module').then( m => m.DetalhesPageModule)
+      },
+      {
+        path: 'products-list',
+        loadChildren: () => import('./products-list/products-list.module').then( m => m.ProductsListPageModule)
+      },
+      {
+        path: 'atualizar-produto/:id',
+        loadChildren: () => import('./atualizar-produto/atualizar-produto.module').then( m => m.AtualizarProdutoPageModule)
       }
     ]
-  } 
+  }
+
 ];
 
 @NgModule({
