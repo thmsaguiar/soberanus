7f1bdb156a9583a44d41ca3c14dfaf6ec895fe74
sothis <21424597@anhembimorumbi.edu.br>
2021-12-06 21:20:31 -0300

    corrigindo bug apk

---
+++ android/app/src/main/java/com/soberanus/MainActivity.java
@@ -0,0 +1,5 @@
+package com.soberanus;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'soberanus-app',
+  appId: 'com.soberanus',
+  appName: 'soberanus',
   webDir: 'www',
   bundledWebRuntime: false
 };
